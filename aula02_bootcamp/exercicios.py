2b61ec87687d5243845ec70de84dc5b89563670e
Naruto-27 <marciana272009@hotmail.com>
2024-09-24 19:06:08 -0300

    inclusão de exercicios

--- aula02_bootcamp/exercicios.py
+++ aula02_bootcamp/exercicios.py
@@ -1,6 +1,20 @@
-import math
+#import math
 
-raio_do_circulo = float(input("Digite o raio"))
-area_do_circulo =  math.pi * raio_do_circulo ** 2
-print(f"{area_do_circulo: .2f}")
+#raio_do_circulo = float(input("Digite o raio"))
+#area_do_circulo =  math.pi * raio_do_circulo ** 2
+#print(f"{area_do_circulo: .2f}")
+
+
+
+
+data_do_usuario =input("Insira uma data no formato dd/mm/aaaa:")
+
+
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[2]}")
+
+
+
 
